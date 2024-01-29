888f823d53503d9357a040e84a8425d46b78390d
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-01-30 00:34:11 +0100

    side-bar content

--- main/resources/views/main.blade.php
+++ main/resources/views/main.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <!-- component -->
+    <!-- Landing page -->
     <section class="h-screen">
         <div class="bg-black text-white py-20">
             <div class="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
@@ -29,8 +29,9 @@
             </div>
         </div>
     </section>
+
     <!-- component -->
-    <div id="product-section" class="px-4 py-16">
+    <div id="product-section" class="px-4 py-16 ">
         <div class="relative w-full md:max-w-2xl md:mx-auto text-center">
             <h1 class="font-bold text-yellow-300 text-xl sm:text-2xl md:text-4xl leading-tight mb-6">
                 A simple and smart Place to get a product
@@ -41,8 +42,16 @@
         </div>
     </div>
     <div class="container mx-auto p-4 lg:h-screen flex items-center justify-center">
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">
             <!-- Blog Entry 1 -->
+            <div class="max-w-sm mx-auto relative shadow-md rounded-lg cursor-pointer">
+                <img src="https://images.unsplash.com/photo-1516205651411-aef33a44f7c2?auto=format&fit=crop&q=80&w=1528&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Img by Meriç Dağlı https://unsplash.com/@meric" class="w-full h-auto object-cover rounded-lg">
+                <div class="absolute bottom-0 left-0 right-0 h-40 bg-black bg-opacity-50 backdrop-blur text-white p-4 rounded-b-lg">
+                    <h1 class="text-2xl font-semibold">Nature Image</h1>
+                    <p class="mt-2">This is a beautiful nature image placeholder. You can replace it with your own image.</p>
+                </div>
+            </div>
+
             <div class="max-w-sm mx-auto relative shadow-md rounded-lg cursor-pointer">
                 <img src="https://images.unsplash.com/photo-1516205651411-aef33a44f7c2?auto=format&fit=crop&q=80&w=1528&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Img by Meriç Dağlı https://unsplash.com/@meric" class="w-full h-auto object-cover rounded-lg">
                 <div class="absolute bottom-0 left-0 right-0 h-40 bg-black bg-opacity-50 backdrop-blur text-white p-4 rounded-b-lg">
