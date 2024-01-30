bb3df50bbed6a8d5f48cbfebb710eb5d01c496a4
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-01-30 02:21:35 +0100

    Product setup

--- main/routes/web.php
+++ main/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 
 /*
@@ -31,14 +32,14 @@ Route::post('/login', [AuthController::class, 'authenticate'])->name('login.auth
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 /* product route */
-Route::get('/product/{product}', [PostController::class, 'show'])->name('products.show');
+Route::get('/product/{product}', [ProductController::class, 'show'])->name('products.show');
 
-Route::post('/product', [PostController::class, 'store'])->name('products.store')->middleware('auth');
+Route::post('/product', [ProductController::class, 'store'])->name('products.store')->middleware('auth');
 
-Route::get('/product/{product}/edit', [PostController::class, 'edit'])->name('products.edit');
+Route::get('/product/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
 
-Route::put('/product/{product}', [PostController::class, 'update'])->name('products.update');
+Route::put('/product/{product}', [ProductController::class, 'update'])->name('products.update');
 
-Route::delete('/product/{product}', [PostController::class, 'destroy'])->name('products.destroy');
+Route::delete('/product/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
 
-Route::post('/product/{product}/comments', [CommentController::class, 'store'])->name('products.comments.store')->middleware('auth');
+//Route::post('/product/{product}/comments', [CommentController::class, 'store'])->name('products.comments.store')->middleware('auth');
