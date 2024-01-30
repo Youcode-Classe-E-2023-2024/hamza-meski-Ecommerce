f398d18b7b02e5f22f1f718b0d3f63164f528515
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-01-30 02:30:40 +0100

    ProductController setup

--- main/app/Http/Controllers/ProductController.php
+++ main/app/Http/Controllers/ProductController.php
@@ -8,5 +8,51 @@ use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function show(Product $product) {
+        return view('products.show-product', compact( 'product'));
+    }
+
+    public function store() {
+        $validated = request()->validate([
+            'content' => 'required|min:5',
+            'title' => 'required|min:5',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048'
+        ]);
+
+        $validated['user_id'] = auth()->user()->id;
+
+        if(request()->hasFile('image')) {
+            $imagePath = request()->file('image')->store('images', 'public');
+            $validated['image'] = $imagePath;
+        }
+
+        Product::create($validated);
+
+        return redirect()->route('main');
+    }
+
+    public function edit(Product $product) {
+        return view('products.edit-product', compact('product'));
+    }
+
+    public function update(Product $product) {
+        $validated = request()->validate([
+            'content' => 'required|min:5|max:200',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048'
+        ]);
+
+        if(request()->hasFile('image')) {
+            $imagePath = request()->file('image')->store('images', 'public');
+            $validated['image'] = $imagePath;
+
+        }
+        $product->update($validated);
+
+        return redirect()->route('products.show', $product->id);
+    }
+
+    public function destroy(Product $product) {
+        $product->delete();
+        return redirect()->route('main');
+    }
 }
