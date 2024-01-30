da901bb1cdd0ad0127c3f1f54c40c6b92dd285fd
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-01-30 12:24:51 +0100

    product details implementation

--- main/resources/views/products/show-product.blade.php
+++ main/resources/views/products/show-product.blade.php
@@ -5,14 +5,14 @@
     <div class="container mx-auto">
         <div class="grid grid-cols-1 md:grid-cols-2 h-screen">
             <div class="max-h-96 md:h-screen">
-                <img class="w-screen h-screen object-cover object-top" src="https://images.pexels.com/photos/270373/pexels-photo-270373.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+                <img class="w-screen h-screen object-cover object-top" src="{{ $product->getImageURL() }}" alt="">
             </div>
             <div class="flex bg-yellow-500 p-10">
                 <div class="mb-auto mt-auto max-w-lg text">
                     <h1 class="text-3xl uppercase bg-black p-2 rounded-md text-center">Jun Santilla</h1>
                     <p class="font mb-5 font-bold">{{ $product->title }}</p>
                     <p class="font-semibold">{{ $product->content }}</p>
-                    <button class="bg-black py-3 px-7 mt-6 text-white">{{ $product->user() }}</button>
+                    <button class="bg-black py-3 px-7 mt-6 text-white">{{ $product->user->name }}</button>
                 </div>
             </div>
         </div>
