8fded373fed132a9bd9e3647b1f7f7da3594fbf9
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-01-30 22:27:26 +0100

    UI

--- main/resources/views/main.blade.php
+++ main/resources/views/main.blade.php
@@ -7,11 +7,11 @@
         <div class="bg-black text-white py-20">
             <div class="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
                 <div class="flex flex-col w-full lg:w-1/3 justify-center items-start p-8">
-                    <h1 class="text-3xl md:text-5xl p-2 text-yellow-300 tracking-loose">TechFest</h1>
-                    <h2 class="text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2">Space : The Timeless Infinity
+                    <h1 class="text-3xl md:text-5xl p-2 text-yellow-300 tracking-loose">Ecommerce</h1>
+                    <h2 class="text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2">Space : The Products Infinity
                     </h2>
-                    <p class="text-sm md:text-base text-gray-50 mb-4">Explore your favourite events and
-                        register now to showcase your talent and win exciting prizes.</p>
+                    <p class="text-sm md:text-base text-gray-50 mb-4">Explore your favourite products and
+                        register now to gain the last exciting stuffs.</p>
                     <a href="#product-section"
                        class="bg-transparent hover:bg-yellow-300 text-yellow-300 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent">
                         Explore Now</a>
