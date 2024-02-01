1ce0125c62ab5ebff212209aed5a0d6f0e3a9a8f
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-02-01 15:36:33 +0100

    icons

--- main/resources/views/layout/header.blade.php
+++ main/resources/views/layout/header.blade.php
@@ -7,6 +7,8 @@
 
     <title>{{ config('app.name') }}</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </head>
 
 <body>
